4a8571199ab1e59ac4a6f455264948ea0af039c4
Rohit R. Mohanty <rohit.r.mohanty@gmail.com>
2019-04-03 17:56:56 +0530

    Updates

---
+++ precache-manifest.6120a131667bb721a8b1653aede5508a.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = [
+  {
+    "revision": "252b48f41d461e7d26a0b6057c563887",
+    "url": "/static/media/section_3.252b48f4.jpg"
+  },
+  {
+    "revision": "be9bb5750fde4d002354397c326af0f7",
+    "url": "/static/media/section_1.be9bb575.png"
+  },
+  {
+    "revision": "42ac5946195a7306e2a5",
+    "url": "/static/js/runtime~main.a8a9905a.js"
+  },
+  {
+    "revision": "45b4af6ce6c227009689",
+    "url": "/static/js/main.16efc7ac.chunk.js"
+  },
+  {
+    "revision": "757dc34f1c8f7d7a92e1",
+    "url": "/static/js/2.a1cbe655.chunk.js"
+  },
+  {
+    "revision": "45b4af6ce6c227009689",
+    "url": "/static/css/main.e6921e30.chunk.css"
+  },
+  {
+    "revision": "0a84f92140f1c4ff249c7d5383d15f73",
+    "url": "/index.html"
+  }
+];
